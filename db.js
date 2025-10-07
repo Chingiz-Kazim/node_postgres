e850ef2cabe497d05c61ea8a31043f7c444716f1
Chingiz-Kazim <kazimov.chingiz.111@gmail.com>
2025-10-07 19:14:58 +0400

    12345

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'postgres',
-    password: 'BakU4114487',
+    password: '*****',
     host: 'localhost',
     port: 5432,
     database: 'postgres'
